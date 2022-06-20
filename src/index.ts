d930c82fc2a03e949175781d02946ef2e2ed33e7
Chris Gradwohl <15326707+cgradwohl@users.noreply.github.com>
2022-06-20 14:58:55 -0700

    neat.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
-const foo = () => console.log("yesss");
+import { highlight } from "cli-highlight";
+const foo = () => {
+  const message = {
+    foo: "bar",
+    baz: {
+      bar: {
+        boo: "bang!",
+      },
+    },
+  };
+  console.log(message);
+  console.log(highlight(JSON.stringify(message, null, 2)));
+};
 foo();
